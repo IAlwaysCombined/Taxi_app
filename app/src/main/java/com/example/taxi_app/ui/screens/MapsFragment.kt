d0dc40ad073b4eae68a184c91c198961a91d5597
Антон Лясников <a.o.lyasnikov@mpt.ru>
2021-05-02 23:45:03 +0300

    Map

--- app/src/main/java/com/example/taxi_app/ui/screens/MapsFragment.kt
+++ app/src/main/java/com/example/taxi_app/ui/screens/MapsFragment.kt
@@ -2,16 +2,22 @@ package com.example.taxi_app.fragments.screens
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.slidingpanelayout.widget.SlidingPaneLayout
 import com.example.taxi_app.R
 import com.example.taxi_app.databinding.FragmentMapsBinding
 import com.example.taxi_app.utilites.APP_ACTIVITY
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.common.internal.service.Common
 import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -19,7 +25,15 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.material.snackbar.Snackbar
 import com.mikepenz.iconics.Iconics.applicationContext
+import kotlinx.android.synthetic.main.activity_auth.*
+import java.io.IOException
+import java.util.*
 
 @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class MapsFragment : Fragment(R.layout.fragment_maps), OnMapReadyCallback {
@@ -28,18 +42,69 @@ class MapsFragment : Fragment(R.layout.fragment_maps), OnMapReadyCallback {
     private lateinit var currentLocation: Location
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val permissionCode = 101
+    val cityname = ""
+
+    private lateinit var  slidingPaneLayout: SlidingPaneLayout
+    private lateinit var  text : TextView
+    private lateinit var autocompleteSupportFragment: AutocompleteSupportFragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentMapsBinding.bind(view)
+        val root = layoutInflater.inflate(R.layout.fragment_maps, main_container)
+        initView(root)
+        init()
     }
 
     override fun onStart() {
         super.onStart()
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(APP_ACTIVITY)
         fetchLocation()
-        APP_ACTIVITY.title = ""
     }
+
+    private fun initView(root: View?){
+        slidingPaneLayout = root?.findViewById(R.id.mapFragment) as SlidingPaneLayout
+        text = root!!.findViewById(R.id.text_test) as TextView
+
+
+    }
+    override fun onLocationResult(p0: LocationResult){
+        setRestrictPlaceInCountry(p0!!.lastLocation)
+    }
+
+    private fun setRestrictPlaceInCountry(lastLocation: Location) {
+        try {
+            val geoCoder = Geocoder(requireContext(), Locale.getDefault())
+            var addressList = geoCoder.getFromLocation(currentLocation.latitude, currentLocation.longitude, 1)
+            if (addressList.size > 0 ) {
+                autocompleteSupportFragment.setCountry(addressList[0].countryCode)
+            }
+        }
+        catch (e: IOException){
+            e.printStackTrace()
+        }
+    }
+
+    private fun init(){
+        Places.initialize(requireContext(),getString(R.string.google_maps_key))
+        autocompleteSupportFragment = childFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID,
+        Place.Field.ADDRESS,
+        Place.Field.LAT_LNG,
+        Place.Field.NAME))
+        autocompleteSupportFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(p0: Place) {
+                Snackbar.make(requireView(),""+ p0.latLng,Snackbar.LENGTH_SHORT).show()
+
+            }
+            override fun onError(p0: Status) {
+                Snackbar.make(requireView(), p0.statusMessage,Snackbar.LENGTH_SHORT).show()
+            }
+
+        })
+
+    }
+
 
     //Fetch user location
     private fun fetchLocation(){
@@ -70,7 +135,7 @@ class MapsFragment : Fragment(R.layout.fragment_maps), OnMapReadyCallback {
         val latLng = LatLng(currentLocation.latitude, currentLocation.longitude)
         val markerOptions = MarkerOptions().position(latLng).title("Я тут!")
         googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng))
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17f))
+        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 100f))
         googleMap.addMarker(markerOptions)
     }
 
@@ -82,4 +147,5 @@ class MapsFragment : Fragment(R.layout.fragment_maps), OnMapReadyCallback {
             }
         }
     }
+
 }
